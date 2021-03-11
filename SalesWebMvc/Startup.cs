c4b3d3b7a8600478cf5149cfa3daea2b4ce51857
Jameson Brandao Martins <jamesonmartins127@gmail.com>
2021-03-11 17:33:23 -0400

    Other entities and second migration

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -25,7 +25,7 @@ namespace SalesWebMvc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<SalesWebMvcDbContext>(options =>
+            services.AddDbContext<SalesWebMvcContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
             );
 
