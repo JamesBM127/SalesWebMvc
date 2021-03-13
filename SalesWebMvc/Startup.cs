8b038e65a6543f6e3ef1926b456019f2422fb49f
Jameson Brandao Martins <jamesonmartins127@gmail.com>
2021-03-13 14:18:23 -0400

    SellerService and basic FindAll

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc
 {
@@ -31,6 +32,7 @@ namespace SalesWebMvc
                     options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
